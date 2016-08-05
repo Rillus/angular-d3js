27f29a0f6fdd48ed1fd4c6049f3a1875bab3bf0c
Rillus <riley@dingdigital.co.uk>
2016-08-05 15:25:01 +0100

    updates where local code lives

--- dist/angular-d3js.js
+++ dist/angular-d3js.js
@@ -14,16 +14,18 @@
 			// and call our onScriptLoad callback when it
 			// has been loaded
 			var scriptTag = $document[0].createElement('script');
-			scriptTag.type = 'text/javascript'; 
+			scriptTag.type = 'text/javascript';
 			scriptTag.async = true;
 			var online = false;
 			if (online) {
 				scriptTag.src = 'http://d3js.org/d3.v3.min.js';
 			} else {
-				scriptTag.src = 'assets/js/d3.min.js';
+				scriptTag.src = 'bower_components/d3/d3.min.js';
 			}
 			scriptTag.onreadystatechange = function () {
-				if (this.readyState == 'complete') onScriptLoad();
+				if (this.readyState === 'complete') {
+                    onScriptLoad();
+                }
 			};
 			scriptTag.onload = onScriptLoad;
 
